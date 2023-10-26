d07e82ee2cf4ec9128a42cbffcbd4b2ec67fa319
Laima <pileliene.laima@gmail.com>
2023-10-26 13:38:18 +0300

    with color

--- script.js
+++ script.js
@@ -3,18 +3,56 @@ console.log("Veikia");
 console.log("Veikia");
 
 const numberSpan = document.querySelector(".span");
-const subtract = document.querySelector(".subtract");
-const reset = document.querySelector(".reset");
-const add = document.querySelector(".add");
+const buttons = document.querySelector(".buttons-conteiner")
 
-subtract.addEventListener("click", () => {
-  numberSpan.innerHTML--;
-});
 
-add.addEventListener("click", () => {
-  numberSpan.innerHTML++;
-});
 
-reset.addEventListener("click", () => {
-  numberSpan.innerHTML = 0;
-});
+buttons.addEventListener("click", (ev) => {
+ console.log("ev:", ev)
+    
+    if(ev.target.classList.contains("subtract")){
+    numberSpan.innerHTML--    
+}
+    if(ev.target.classList.contains("add")){
+    numberSpan.innerHTML++
+    }
+    if(ev.target.classList.contains("reset")){
+    numberSpan.innerHTML = 0
+    }
+   setColor()
+})
+
+
+
+const setColor = ()=>{
+    if(numberSpan.innerHTML>0){
+        numberSpan.style.color="yellow"
+}
+else if(numberSpan.innerHTML<0){
+    numberSpan.style.color="red"
+}
+else{
+    numberSpan.style.color="white"
+
+}
+}
+
+
+
+
+// const subtract = document.querySelector(".subtract");
+// const reset = document.querySelector(".reset");
+// const add = document.querySelector(".add");
+
+
+// subtract.addEventListener("click", () => {
+//   numberSpan.innerHTML--;
+// });
+
+// add.addEventListener("click", () => {
+//   numberSpan.innerHTML++;
+// });
+
+// reset.addEventListener("click", () => {
+//   numberSpan.innerHTML = 0;
+// });
